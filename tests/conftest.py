9e0b44552aa1d86a7c239fe9969ff207eafe2568
tooweirdg <gulshansharma7576+github@gmail.com>
2026-06-18 18:00:53 +0530

    tests: initialize database schema in CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,16 @@
+import pytest
+
 from app.db.base import Base
 from app.db.session import engine
 
-Base.metadata.create_all(bind=engine)
+from app.models.log import Log
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+
+    Base.metadata.create_all(bind=engine)
+
+    yield
+
+    Base.metadata.drop_all(bind=engine)

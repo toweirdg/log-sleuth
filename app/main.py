8cb4102890c44922d4dce2e9e8f5a38ae21d173d
tooweirdg <gulshansharma7576+github@gmail.com>
2026-06-21 14:45:10 +0530

    fix: initialize FastAPI app before registering CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
+from prometheus_client import generate_latest
+
 from app.api.routes import logs
 from app.api.routes import health
-from fastapi.responses import PlainTextResponse
-from prometheus_client import generate_latest
-from fastapi.middleware.cors import CORSMiddleware
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 app = FastAPI(
@@ -19,6 +12,19 @@ app = FastAPI(
     description="Asynchronous log ingestion and analysis platform",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(logs.router)
 app.include_router(health.router)
 
